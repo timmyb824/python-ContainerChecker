d15a661a5c886076ce1167bae93a954578b03489
Tim Bryant <timothybryant3@gmail.com>
2024-08-06 03:36:11 -0400

    feat: Add argument parser for command line arguments

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
+import argparse
+
 from src.config import read_yaml
+from src.log_handler import setup_logging
 from src.ssh import process_server
 
 
-def main():
-    servers = read_yaml("servers.yaml")["servers"]
+def parse_args():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Process servers and check for running containers."
+    )
+    parser.add_argument(
+        "--file", type=str, default="servers.yaml", help="Path to servers.yaml"
+    )
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    return parser.parse_args()
 
-    for server in servers:
+
+def main():
+    """Main function."""
+    args = parse_args()
+
+    logger = setup_logging(args.verbose)
+
+    if args.verbose:
+        logger.debug("Verbose output enabled")
+
+    servers = read_yaml(args.file)
+
+    if servers is None:
+        logger.error(f"Failed to read servers from {args.file}")
+        return
+
+    for server in servers.get("servers", []):
         host = server["host"]
         port = server.get("port", 22)
         user = server["user"]
